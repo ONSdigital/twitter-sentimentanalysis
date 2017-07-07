ce8bd21fcdae9cb56b4e0efe44dbb3eead645a87
jenks2017 <Joseph.Jenkins@ons.gov.uk>
2017-07-07 14:31:23 +0100

    added some filtering to the code

--- import_tweets.R
+++ import_tweets.R
@@ -1,1 +1,7 @@
-tweets<-read.csv("C:/Users/Joseph Jenkins/Documents/twit_13Aug14_sample.csv", header = F, fileEncoding = "UTF-16LE")
+library(dplyr)
+
+tweets<-read.csv("C:/Users/Joseph Jenkins/Documents/twit_13Aug14_1Ksample.csv", header = F, fileEncoding = "UTF-16LE")%>%
+  select(V3, V4, V6, V7, V8, V9, V10)%>%
+  filter(V4 =="en" & V8 !="NA" & V9 !="NA")
+
+
