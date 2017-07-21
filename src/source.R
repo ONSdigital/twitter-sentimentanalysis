da6494afd75d69bb33265b88798049647624eb27
Ross Bowen <ross.bowen@ons.gov.uk>
2017-07-21 13:41:37 +0100

    Generalised source script to allow iteration over many files, and put analysis bits in a new script

--- src/source.R
+++ src/source.R
@@ -2,14 +2,17 @@ library(rtweet)
 library(tidyverse)
 library(tidytext)
 
+process_data <- function(username, input){
+
 #DATA CLEANING AND PREPARATION ---------------------------------------------------------
 
-data <- read.csv("C:\\Users\\Ross Bowen\\Documents\\chunck0.uniq.csv", 
-                 header = F, 
+data <- read.csv(paste0("C:\\Users\\", username, "\\Documents\\chunck", input, ".uniq.csv"), 
+                 header = TRUE, 
                  stringsAsFactors = FALSE)
 
 data <- data %>% 
-  filter(language == "en")
+  filter(language == "en") %>%
+  mutate(ID = row_number())
 
 #Function which cleans tweets (maybe shorten later?)
 clean_tweet <- function(text){
@@ -32,82 +35,22 @@ tokenised_data <- cleaned_data %>% unnest_tokens(input = text, output = "word", 
 #SENTIMENT SCORING ---------------------------------------------------------------------
 
 scored_data_afinn <- tokenised_data %>%
-  inner_join(get_sentiments("afinn")) %>%
-  group_by(V2) %>%
-  summarise(afinn = sum(score)) %>% full_join(cleaned_data)
+  inner_join(get_sentiments("afinn"), by = "word") %>%
+  group_by(ID) %>%
+  summarise(afinn = sum(score)) %>% 
+  full_join(cleaned_data, by = "ID") %>%
+  select(-ID)
 
-scored_data_bing <- tokenised_data %>%
-  inner_join(get_sentiments("bing")) %>%
-  mutate(score = case_when(sentiment == "positive" ~ 1, 
-                                     sentiment == "negative" ~ -1)) %>%
-  group_by(V2) %>%
-  summarise(bing = sum(score)) %>% full_join(cleaned_data)
+#scored_data_bing <- tokenised_data %>%
+#  inner_join(get_sentiments("bing")) %>%
+#  mutate(score = case_when(sentiment == "positive" ~ 1, 
+#                                     sentiment == "negative" ~ -1)) %>%
+#  group_by(V2) %>%
+#  summarise(bing = sum(score)) %>% full_join(cleaned_data)
 
-#SENTIMENT ANALYSIS --------------------------------------------------------------------
+#OUTPUT --------------------------------------------------------------------------------
 
-#AFINN
+write.csv(scored_data_afinn, paste0("C:\\Users\\", username, "\\Documents\\chunck", input, ".uniq.csv"),
+          row.names = FALSE)
 
-#Most frequent positive words, afinn
-tokenised_data %>%
-  inner_join(get_sentiments("afinn")) %>%
-  filter(score > 0) %>% 
-  count(word, sort = TRUE) %>%
-  inner_join(get_sentiments("afinn"))
-
-#Most frequent negative words, afinn
-tokenised_data %>%
-  inner_join(get_sentiments("afinn")) %>%
-  filter(score < 0) %>% 
-  count(word, sort = TRUE) %>%
-  inner_join(get_sentiments("afinn"))
-
-#BING
-
-pos <- get_sentiments(lexicon = "bing") %>%
-  filter(sentiment == "positive")
-
-neg <- get_sentiments(lexicon = "bing") %>%
-  filter(sentiment == "negative")
-
-neg_sentiment <- tokenized_tweets %>%
-  inner_join(neg) %>%
-  count(word, sort=TRUE)
-
-pos_sentiment <- tokenized_tweets %>%
-  inner_join(pos) %>%
-  count(word,sort = TRUE)
-
-tokenized_tweets %>%
-  inner_join(get_sentiments("bing")) %>%
-  count(sentiment) %>%
-  spread(sentiment, n, fill = 0) %>%
-  mutate(sentiment = positive - negative)
-
-
-#Week-by-week breakdown of stretch "topics"
-
-#FRIDAY: This code (prior to analysis - i.e., data formation) should be turned into a function 
-#(input = .csv, output = .csv with sentiment scores)
-
-#Hadoop - generalise to huge data.
-
-
-#WORDCLOUD // broken by NRC?
-#Geography, differences in UK, linked with poverty/deprivation
-#How do you go from coordinates to geographies?
-#Scatter plot superimposed on UK map.
-#Time series analysis, Fourier analysis - morning/evening, weekday/weekend, holidays etc.
-
-#Identify events circa 2014 which may have influenced sentiment
-#ONS Wellbeing metrics comparisons.
-
-#Worried lexicon?
-
-#NRC PCA (which sentiments most separate the population)
-#NRC Wellbeing index (weight sentiments according to PCA/Factors)
-
-
-
-
-
-
+}
